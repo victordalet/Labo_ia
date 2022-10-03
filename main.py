78bde44431861b705a515c08d1f2859d55fe7ba7
unknown <victordalet@protonmail.com>
2022-10-03 21:46:12 +0200

    poo

--- main.py
+++ main.py
@@ -5,53 +5,50 @@
 
 import cv2
 
-face_detector = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-WHITE = (255, 255, 255)
-KNOWN_DISTANCE = 20  # centimeter
-KNOWN_WIDTH = 18  # centimeter
-fonts = cv2.FONT_HERSHEY_COMPLEX
+class Facedetector:
+    def __init__(self):
+        self.face_detector = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+        self.WHITE = (255, 255, 255)
+        self.KNOWN_DISTANCE = 20  # centimeter
+        self.KNOWN_WIDTH = 18  # centimeter
+        self.fonts = cv2.FONT_HERSHEY_COMPLEX
 
-def focal_length(measured_distance, real_width, width_in_rf_image):
-    focal_length_value = (width_in_rf_image * measured_distance) / real_width
-    return focal_length_value
+    def focal_length(self):
+        self.focal_length_found = (self.ref_image_face_width * self.KNOWN_DISTANCE) / self.KNOWN_WIDTH
 
-def distance_finder(focal_length, real_face_width, face_width_in_frame):
-    distance = (real_face_width * focal_length) / face_width_in_frame
-    return distance
+    def distance_finder(self):
+        self.distance = (self.KNOWN_WIDTH * self.focal_length_found) / self.face_width_in_frame
 
-def face_data(image):
-    face_width = 0
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    faces = face_detector.detectMultiScale(gray_image, 1.3, 5)
-    for (x, y, h, w) in faces:
-        cv2.rectangle(image, (x, y), (x + w, y + h), WHITE, 1)
-        face_width = w
+    def face_data(self,image):
+        face_width = 0
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        faces = self.face_detector.detectMultiScale(gray_image, 1.3, 5)
+        for (x, y, h, w) in faces:
+            cv2.rectangle(image, (x, y), (x + w, y + h), self.WHITE, 1)
+            face_width = w
+        return face_width
 
-    return face_width
+    def run(self):
 
-def main():
-
-    ref_image = cv2.imread("Ref_image.png")
-    ref_image_face_width = face_data(ref_image)
-    focal_length_found = focal_length(KNOWN_DISTANCE, KNOWN_WIDTH, ref_image_face_width)
+        self.ref_image = cv2.imread("Ref_image.png")
+        self.ref_image_face_width = self.face_data(self.ref_image)
+        self.focal_length()
 
 
-    cap = cv2.VideoCapture(0)
-    while True:
-        _, frame = cap.read()
-        face_width_in_frame = face_data(frame)
-        if face_width_in_frame != 0:
-            print(focal_length_found)
-            Distance = distance_finder(focal_length_found, KNOWN_WIDTH, face_width_in_frame)
-            print(Distance)
-            cv2.putText(
-                frame, f"Distance = {round(Distance, 2)} CM", (50, 50), fonts, 1, (WHITE), 2
-            )
+        self.cap = cv2.VideoCapture(0)
+        while True:
+            _, self.frame = self.cap.read()
+            self.face_width_in_frame = self.face_data(self.frame)
+            if self.face_width_in_frame != 0:
+                self.distance_finder()
+                cv2.putText(
+                    self.frame, f"Distance = {round(self.distance, 2)} CM", (50, 50), self.fonts, 1, (self.WHITE), 2
+                )
+                
+            cv2.imshow("frame", self.frame)
+            if cv2.waitKey(1)==ord("q"):
+                break
+        self.cap.release()
+        cv2.destroyAllWindows()
 
-        cv2.imshow("frame", frame)
-        if cv2.waitKey(1)==ord("q"):
-            break
-    cap.release()
-    cv2.destroyAllWindows()
-
-main()
+Facedetector().run()
